c0d507355bb15b4d7daba628935982bcee905235
moacirdeveloper <106824200+moacirdeveloper@users.noreply.github.com>
2022-09-12 11:44:40 -0300

    Atualizando arquivo
    arquivo app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
+const botao = document.getElementById("cmdMenu");
+const nav = document.getElementsByClassName("nav");
 
+botao.addEventListener("click", abrirMenu);
+
+function abrirMenu(){
+
+    nav[0].classList.toggle("active");
+
+}
